b34c4479c4f5eccd08e47e592af231b021b65077
anthonyx21 <anthonyx21@gmail.com>
2020-07-25 17:27:31 -0400

    add rest api

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
   mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
 end
+# rails routes
